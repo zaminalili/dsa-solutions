6ec9a17a5f65a295e28b811c3b445caff4110d02
zaminalili <zaminalili29@gmail.com>
2025-02-12 11:51:32 +0400

    Customize linkedlist

--- CSharp/DataStuctures/LinkedList.cs
+++ CSharp/DataStuctures/LinkedList.cs
@@ -1,10 +1,114 @@
-public class ListNode 
+namespace CSharp.DataStuctures;
+
+public class Node<T>
 {
-    public int val;
-    public ListNode next;
-    public ListNode(int val=0, ListNode next=null) 
+    public T val;
+    public Node<T> next;
+    public Node(T val=default!, Node<T> next=null!) 
     {
         this.val = val;
         this.next = next;
     }
 }
+
+
+public class MyLinkedList<T> 
+{
+    private Node<T>? Head {get; set;}
+    private Node<T>? Tail {get; set;}
+
+    public MyLinkedList()
+    {
+        Head = null;
+        Tail = null;
+    }
+
+    public Node<T> AddFirst(T value) 
+    {
+        Head = new Node<T>(value, Head!);
+        if(Tail == null)
+            Tail = Head;
+
+        return Head;
+    }
+
+    public void AddFirst(Node<T> node) 
+    {
+        Head = node;
+    }
+
+    public Node<T> AddLast(T value) 
+    {
+        Node<T> newNode = new Node<T>(value);
+
+        if(Head == null)
+            Head = newNode;
+
+        else if(Tail == null)
+        {
+            Tail = newNode;
+            Head.next = Tail;
+        }
+        else 
+        {
+            var tempNode = Tail;
+            Tail = newNode;
+            tempNode.next = Tail;
+        }
+
+        return newNode;
+    }
+
+    public void AddLast(Node<T> node) 
+    {
+        if(Head == null)
+            AddFirst(node);
+
+        else if(Tail == null)
+        {
+            Tail = node;
+            Head.next = Tail;
+        }
+        else 
+        {
+            var tempNode = Tail;
+            Tail = node;
+            tempNode.next = Tail;
+        }
+            
+    }
+
+    public Node<T> AddAfter(Node<T> node, T value) 
+    {
+        node.next = new Node<T>(value, node.next);
+
+        return node.next;
+    }
+
+    public void AddAfter(Node<T> node, Node<T> newNode) 
+    {
+        node.next = newNode;
+    }
+
+    // public Node<T> AddBefore(Node<T> node, T value) 
+    // {
+    //     new Node<T> (value, node);
+    // }
+
+    public void WriteAllValue() 
+    {
+        var current = Head;
+
+        if(current != null) 
+        {
+            while(current != null)
+            {
+                Console.Write($"{current.val} -> ");
+                current = current.next;
+            }
+        }
+            
+        
+    }
+}
+
--- CSharp/Program.cs
+++ CSharp/Program.cs
@@ -1,17 +1,50 @@
-﻿using CSharp.Easy.Stackes;
+﻿using CSharp.DataStuctures;
 
-var validParantheses = new ValidParentheses();
+Console.WriteLine("");
+LinkedList<int> ints = new();
 
-string input1 = "()";
-string input2 = "()[]{}";
-string input3 = "(]";
-string input4 = "([])";
-string input5 = "]";
+var head = ints.AddLast(1);
+var curr = ints.AddLast(2);
+ints.AddLast(3);
 
-Console.WriteLine(validParantheses.IsValid(input1));
-Console.WriteLine(validParantheses.IsValid(input2));
-Console.WriteLine(validParantheses.IsValid(input3));
-Console.WriteLine(validParantheses.IsValid(input4));
-Console.WriteLine(validParantheses.IsValid(input5));
 
-// Console.WriteLine($"Is pair: {validParantheses.IsPair('(', ']')}");
+// foreach (var item in ints)
+// {
+//     Console.WriteLine(item.ToString());
+// }
+
+MyLinkedList<int> list = new();
+
+// System.Console.WriteLine(list.AddLast(1).val);
+var node = list.AddLast(1);
+list.AddLast(2);
+list.AddLast(3);
+list.AddLast(4);
+
+list.AddAfter(node, 100);
+
+
+
+list.WriteAllValue();
+
+
+
+// ListNode<int> node4 = new ListNode<int>(4);
+// ListNode<int> node3 = new ListNode<int>(3, node4);
+// ListNode<int> node2 = new ListNode<int>(2, node3);
+// ListNode<int> node1 = new ListNode<int>(1);
+
+// var head = node1;
+
+// head.AddValueToNext(2);
+// head.AddValueToNext(3);
+// head.AddValueToNext(4);
+// head.AddValueToNext(5);
+
+// 1 -> 5 -> 4 -> 3 -> 2
+
+// while (head.next != null) 
+// {
+//     Console.WriteLine(head.val);
+//     head = head.next;
+// }
