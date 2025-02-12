0a561ebe5c64cd72e3815bcc140a5f7d783cec6f
zaminalili <zaminalili29@gmail.com>
2025-02-12 11:53:51 +0400

    Refolder ReverseLinkedList solution

--- CSharp/Easy/LinkedLists/206. ReverseLinkedList.cs
+++ CSharp/Easy/LinkedLists/206. ReverseLinkedList.cs
@@ -1,5 +1,16 @@
 namespace CSharp.Easy.Array;
 
+public class ListNode
+{
+    public int val;
+    public ListNode next;
+    public ListNode(int val=0, ListNode next=null!) 
+    {
+        this.val = val;
+        this.next = next;
+    }
+}
+
 public class ReverseLinkedList
 {
     public ListNode ReverseList(ListNode head)
