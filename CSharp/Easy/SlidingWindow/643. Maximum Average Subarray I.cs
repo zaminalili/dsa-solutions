fc8bdf9c8580c35a0bcebd9f80ebffa7960a4c97
zaminalili <zaminalili29@gmail.com>
2025-02-18 18:01:47 +0400

    Optimize solution 643

--- CSharp/Easy/SlidingWindow/643. Maximum Average Subarray I.cs
+++ CSharp/Easy/SlidingWindow/643. Maximum Average Subarray I.cs
@@ -4,18 +4,28 @@ public static class MaximumAverageSubarray
 {
     public static double FindMaxAverage(int[] nums, int k) 
     {
-        var sum = nums.Take(k).Sum();
-        double average, maxAverage = sum / (double)k;
-
+        int sum = Sum(nums, 0, k);
+        //double average, maxAverage = sum / (double)k;
+        int maxSum = sum;
         for (int i = k; i < nums.Length; i++)
         {
             sum -= nums[i-k] - nums[i];
-            average = sum / (double)k;
-
-            if(average > maxAverage)
-                maxAverage = average;
+            //average = sum / (double)k;
+            if(sum > maxSum)
+                maxSum = sum;
         }
 
-        return maxAverage;    
+        return maxSum / (double)k;    
+    }
+    
+    private static int Sum(int[]nums, int firstIndex, int lastIndex) 
+    {
+        int sum = 0;
+        for (int i = firstIndex; i < lastIndex; i++)
+        {
+            sum += nums[i];
+        }
+
+        return sum;
     }
 }
