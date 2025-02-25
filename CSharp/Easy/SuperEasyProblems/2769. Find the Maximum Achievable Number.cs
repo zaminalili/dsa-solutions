e5faf5bfeb9215761f635a704f7f3acbbd76d86f
zaminalili <zaminalili29@gmail.com>
2025-02-25 17:52:27 +0400

    Solve 2769. Find the Maximum Achievable Number

---
+++ CSharp/Easy/SuperEasyProblems/2769. Find the Maximum Achievable Number.cs
@@ -0,0 +1,9 @@
+namespace CSharp.Easy.SuperEasyProblems;
+
+public class FindTheMaximumAchievableNumber
+{
+    public int TheMaximumAchievableX(int num, int t) 
+    {
+        return num + 2*t;    
+    }
+}
