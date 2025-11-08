9c20de122762acbb5297a8036bd467acf4a6da53
zaminalili <zaminalili@proton.me>
2025-11-08 21:50:36 +0400

    Optimize 771

--- CSharp/Easy/HashMap/771. Jewels and Stones.cs
+++ CSharp/Easy/HashMap/771. Jewels and Stones.cs
@@ -2,23 +2,14 @@ public class Solution771
 {
     public int NumJewelsInStones(string jewels, string stones) 
     {
-        var myJewels = new Dictionary<char, int>();
+        var result = 0;
 
         foreach (var stone in stones)
         {
             if(jewels.Contains(stone))
-            {
-                if(myJewels.TryGetValue(stone, out int count))
-                {
-                    myJewels[stone] = count+1;
-                }
-                else 
-                {
-                    myJewels[stone] = 1;
-                }
-            }
+                result++;
         }
 
-        return myJewels.Values.Sum();
+        return result;
     }
 }
